339066596081bb0a3c8f3c22b9098a448cb91054
Paolo Flores <pflores.fisi22@gmail.com>
2023-10-08 01:26:30 -0500

    refactor(articles): migrate to class-based views

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('', views.articles_list),
-    path('<int:pk>/', views.article_detail)
+    path('', views.ArticleList.as_view()),
+    path('<int:pk>/', views.ArticleDetail.as_view())
 ]
