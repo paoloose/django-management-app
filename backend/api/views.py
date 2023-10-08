b0be16aa2da418bed8c51fcbed8c47f7910f13ec
Paolo Flores <pflores.fisi22@gmail.com>
2023-10-08 18:10:05 -0500

    feat: setup api index

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpRequest
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def api_home(_: HttpRequest):
     return JsonResponse({
         'status': 'UP'
