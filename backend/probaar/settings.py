323504e3364ecdd54186b62cdb4dc97d92bf9f01
Paolo Flores <pflores.fisi22@gmail.com>
2023-10-08 19:14:23 -0500

    feat: setup Swagger UI integration

--- backend/probaar/settings.py
+++ backend/probaar/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'probaar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/probaar/urls.py
+++ backend/probaar/urls.py
@@ -5,8 +5,24 @@ https://docs.djangoproject.com/en/4.2/topics/http/urls/
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+swagger_view = TemplateView.as_view(
+    template_name='swagger-ui.html',
+    extra_context={'schema_url': 'openapi-schema'}
+)
+
+openapi_view = get_schema_view(
+    title='Probaar\'s API',
+    description='API Swagger endpoint for easily testing!',
+    version='0.0.1',
+    permission_classes=[]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('openapi/', openapi_view, name='openapi-schema'),
+    path('', swagger_view, name='swagger-ui'),
 ]
